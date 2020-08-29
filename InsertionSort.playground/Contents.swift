112d421147cb42faccb944e08060f591af3c404b
Henrique Abreu <abreu-semperfi@hotmail.com>
2020-08-29 11:35:11 +0100

    edit: created class

--- InsertionSort.playground/Contents.swift
+++ InsertionSort.playground/Contents.swift
@@ -1,35 +1,38 @@
 import Foundation
 
-func insertSort(toSort: [Int]) -> [Int]{
-    
-    var result: [Int] = []
-    result.append(toSort[0])
-    
-    for j in 1..<toSort.count{
-        let key = toSort[j]
-        result.insert(key, at: j)
+class InsertionSort{
+    public func insertSort(toSort: [Int]) -> [Int]{
         
-        var i = j - 1
+        var result: [Int] = []
+        result.append(toSort[0])
         
-        while i >= 0 && result[i] > key{
-            result[i + 1] = result[i]
-            i -= 1
-            result[i + 1] = key
+        for j in 1..<toSort.count{
+            let key = toSort[j]
+            result.insert(key, at: j)
+            
+            var i = j - 1
+            
+            while i >= 0 && result[i] > key{
+                result[i + 1] = result[i]
+                i -= 1
+                result[i + 1] = key
+            }
+        }
+        
+        return result
+    }
+
+
+    public func testToSort() -> String{
+        let test = [0, 0, 3, 2, 1, -1, 500, -1, 10]
+        if insertSort(toSort: test) == [-1, -1, 0, 0, 1, 2, 3, 10, 500]{
+            return "Test Passed"
+        } else{
+            return "Test Failed"
         }
     }
-    
-    return result
+
+   
 }
 
-
-func testToSort() -> String{
-    let test = [0, 0, 3, 2, 1, -1, 500, -1, 10]
-    if insertSort(toSort: test) == [-1, -1, 0, 0, 1, 2, 3, 10, 500]{
-        return "Test Passed"
-    } else{
-        return "Test Failed"
-    }
-}
-
-print(testToSort())
-
+print(InsertionSort().testToSort())
